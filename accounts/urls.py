b1106d21a584ad61d47be6a1be4783f2cefca080
Neal Guarddin <guarddin29@gmail.com>
2025-11-25 23:06:31 +0700

    Sudah updated bagian API Neal

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 # accounts/urls.py
 from django.urls import path
-from .views import (edit_profile_api, register, login_user, logout_user, home, 
+from .views import (admin_dashboard_api, delete_account_api, edit_profile_api, register, login_user, logout_user, home, 
                     profile_detail, profile_edit, delete_account, delete_avatar, 
                     admin_dashboard, login_api, register_api, user_profile_api_json, logout_api)
 
@@ -9,19 +9,12 @@ app_name = 'accounts'
 urlpatterns = [
     path('', home, name='home'),
 
+    # ========== KEBUTUHAN WEB ==========
     # Untuk mengarahkan ke bagian web 
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('delete/', delete_account, name='delete'),
-
-    # Untuk mengarahkan ke mobile/JSON Response/API
-    # path("api/register/", register, name="api_register"),
-    path('api/login/', login_api, name='login_api'),
-    path('api/register/', register_api, name='register_api'),
-    path('api/logout/', logout_api, name='logout_api'),
-    path('api/profile/json/', user_profile_api_json, name='profile_api'),
-    path('api/profile/edit/', edit_profile_api, name='edit_profile_api'),   
 
     # >>> statis lebih dulu
     path('profile/edit/', profile_edit, name='profile_edit'),
@@ -31,5 +24,21 @@ urlpatterns = [
     path('profile/<str:username>/', profile_detail, name='profile_detail'),
 
     # Admin
-    path("admin/", admin_dashboard, name="admin_dashboard")
+    path("admin/", admin_dashboard, name="admin_dashboard"),
+    #
+
+    # ========== KEBUTUHAN MOBILE/JSON RESPONSE/API ==========
+    # Untuk mengarahkan ke mobile/JSON Response/API
+    path('api/login/', login_api, name='login_api'),
+    path('api/register/', register_api, name='register_api'),
+    path('api/logout/', logout_api, name='logout_api'),
+    path('api/profile/json/', user_profile_api_json, name='profile_api'),
+    path('api/profile/edit/', edit_profile_api, name='edit_profile_api'),
+    path('api/profile/delete/', delete_account_api, name='delete_account_api'),
+    path('api/admin/dashboard/', admin_dashboard_api, name='admin_dashboard_api'),
+
+    # ========== AKHIR KEBUTUHAN MOBILE/JSON RESPONSE/API ==========
+
+
+    
 ]
