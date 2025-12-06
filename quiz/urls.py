41c5a778ee63d7e60c44ae0cb7c114da10100b4d
Hannan-Afif <hannanafifd@gmail.com>
2025-12-06 21:24:34 +0700

    Removing redundant code

--- quiz/urls.py
+++ quiz/urls.py
@@ -13,31 +13,28 @@ from quiz.views import (
     submit_quiz,
     quiz_admin_detail,
     quiz_admin_quizzez,
-    
-    # Flutter APIs
     create_quiz_flutter,
     edit_quiz_flutter,
     delete_quiz_flutter,
-    get_quiz_for_edit_flutter, # Add this
+    get_quiz_for_edit_flutter, 
 )
 
 app_name = 'quiz'
 
 urlpatterns = [
+    # APIs
     path('', show_main, name='show_main'),
     path('api/', get_all_quizzes, name="get_all_quizzes"),
     path('api/admin/', quiz_admin_quizzez, name="get_admin_quizzes"),
     path('api/<int:quiz_id>/', get_quiz_detail, name="get_quiz_detail"), 
     path('api/<int:quiz_id>/submit/', submit_quiz, name="submit_quiz"),   
     path('api/<int:quiz_id>/admin/', quiz_admin_detail, name="quiz_admin_detail"),  
-
-    # FLUTTER APIS
     path('api/create-flutter/', create_quiz_flutter, name='create_quiz_flutter'),
     path('api/edit-flutter/<int:quiz_id>/', edit_quiz_flutter, name='edit_quiz_flutter'),
     path('api/delete-flutter/<int:quiz_id>/', delete_quiz_flutter, name='delete_quiz_flutter'),
-    path('api/quiz-data/<int:quiz_id>/', get_quiz_for_edit_flutter, name='get_quiz_for_edit_flutter'), # New URL
+    path('api/quiz-data/<int:quiz_id>/', get_quiz_for_edit_flutter, name='get_quiz_for_edit_flutter'), 
 
-    # WEB APIS
+    # Web routing
     path('create/', create_quiz_with_questions, name='create_quiz'),
     path('<int:quiz_id>/', quiz_detail, name='quiz_detail'),
     path('<int:quiz_id>/take/', take_quiz, name='take_quiz'),
